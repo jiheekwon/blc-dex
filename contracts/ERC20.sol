aa00f994c8818b25bf6bbc86c7270c61406c8053
jiheekwon <konn3444@gmail.com>
2023-08-12 22:55:11 +0900

    update ERC20.sol

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -8,11 +8,24 @@ contract ERC20 {
   uint256 public totalSupply;
 
   mapping(address => uint256) private balances;
+  mapping(address => mapping(address => uint256)) private allowances;
 
   event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+  constructor(string memory _name, string memory _symbol, uint256 _totalSupply){
+    name = _name;
+    symbol = _symbol;
+    totalSupply = _totalSupply;
+    balances[msg.sender] = totalSupply;
+  }
 
   function balanceOf(address _owner) external view returns (uint256) {
     return balances[_owner];
+  }
+
+  function allowance(address _owner, address _spender) public view returns (uint256){
+    return allowances[_owner][_spender];
   }
 
   function transfer(address _to, uint256 _value) external returns (bool){
@@ -21,7 +34,15 @@ contract ERC20 {
   }
 
   function transferFrom(address _from, address _to, uint256 _value) external returns (bool) {
+    require(allowances[_from][msg.sender] >= _value, "Transfer amount exceeds allowance");
     _transfer(_from, _to, _value);
+    allowances[_from][msg.sender] -= _value;
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) public returns (bool){
+    allowances[msg.sender][_spender] = _value;
+    emit Approval(msg.sender, _spender, _value);
     return true;
   }
 
